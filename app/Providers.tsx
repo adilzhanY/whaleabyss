4b3a1615958c076dd0ed86c5964dfaef5b6bddb8
adilzhanY <adilzhanit@gmail.com>
2026-05-14 22:00:14 +0200

    fix: let open cart on every page of the website

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -2,12 +2,14 @@
 
 import { SessionProvider } from "next-auth/react";
 import CartSync from "@/components/CartSync";
+import CartModal from "@/components/CartModal";
 import FloatingBanner from "@/components/FloatingBanner";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
       <CartSync />
+      <CartModal />
       <FloatingBanner />
       {children}
     </SessionProvider>

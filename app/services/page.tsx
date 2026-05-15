b9c5a756e8953ce4d73dcf10bbe9613402008f1b
adilzhanY <adilzhanit@gmail.com>
2026-05-15 14:57:54 +0200

    fix services page by removing cache and always pulling updates from db

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,6 +1,8 @@
 import { getServiceCategories } from "@/lib/services";
 import ServicesClient from "./ServicesClient";
 
+export const revalidate = 0;
+
 export default async function ServicesPage() {
   const categories = await getServiceCategories();
   return <ServicesClient categories={categories} />;

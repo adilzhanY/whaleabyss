c82ae34f598842bf44ed7ef5b56b53a468e49a35
adilzhanY <adilzhanit@gmail.com>
2026-07-28 15:12:49 +0200

    chore: tidy tsconfig and gitignore lavish artifacts

--- app/admin/orders/[id]/DeleteOrderButton.tsx
+++ app/admin/orders/[id]/DeleteOrderButton.tsx
@@ -126,6 +126,7 @@ export default function DeleteOrderButton({
               >
                 Отмена
               </button>
+              
               <button
                 onClick={handleDelete}
                 disabled={isDeleting || !canDelete}
